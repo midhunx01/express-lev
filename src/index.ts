df5c638d38d64a70b04ef4dc10cf27c8cf36ce8b
midhunx01 <midhunxda.123@gmail.com>
2025-10-21 17:45:51 +0530

    fix: update export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-export const logger = "./util/logger/index.js";
-export const httpLogger = "./util/logger/index.js";
-export const validateRequest = "./util/validator.js";
+export {
+  APIError,
+  ValidationError,
+  AuthorizeError,
+  NotFoundError,
+} from "./util/error/errors";
+export { STATUS_CODES } from "./util/error/status-codes";
+export {
+  HandleErrorWithLogger,
+  HandleUnCaughtException,
+} from "./util/error/handler";
+export { ValidateError } from "./util/error/validator";
+
+// Logger exports
+export { logger, httpLogger } from "./util/logger/index";
+
+// Validator exports
+export { ValidateRequest, sanitizeStrings } from "./util/validator";
